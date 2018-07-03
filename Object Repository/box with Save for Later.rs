f70d14554df61fb02c5dfb29ccc18db020153952
mashtakovamary <mariamashtakova1996@gmail.com>
2018-07-03 15:30:08 +0300

    verify that product was added to the wishlist

---
+++ Object Repository/box with Save for Later.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>box with Save for Later</name>
+   <tag></tag>
+   <elementGuidId>af5f9606-c675-4d52-8823-6dd465d6db15</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(text(),'Save for Later')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
